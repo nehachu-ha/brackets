4722e0bf7cd72b8056173651983e2e10bf7ffad3
Irina Sharaeva <irasharaeva@gmail.com>
2021-03-09 13:36:10 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2 !== 0) {
+        return false;
+    }
+    else {
+        for (let i = str.length - 1; i >= 0; i--) { //проверка совпадения элемента строки и элемента конфига скобок
+            for (let j = 0; j < bracketsConfig.length; j++)
+                if (str[i] === bracketsConfig[j][0]) { //берем первый элемент матрицы - это массив а в нем берем первый элемент массива
+                    str = str.replace(bracketsConfig[j].join(''), ''); //собираем снова в строку получившийся массив
+                }
+            }
+        }
+
+        return str === '';
+
+    }
 }
